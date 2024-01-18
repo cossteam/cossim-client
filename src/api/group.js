305fa7abc39ebf20e7185cad71b9fc10d3e5896e
minisailboat <minisailboat@qq.com>
2024-01-18 15:04:58 +0800

    feat：管理加入群聊

--- src/api/group.js
+++ src/api/group.js
@@ -3,10 +3,28 @@ import request from '@/utils/request'
 // const baseApi = '/group'
 const relationGroup = '/relation/group'
 
+/**
+ * 获取群申请列表
+ * @param {*} params
+ * @returns
+ */
 export function groupRequestListApi(params) {
 	return request({
 		url: relationGroup + '/request_list',
 		method: 'GET',
 		params
+	})
+}
+
+/**
+ * 管理加入群聊
+ * @param {*} data
+ * @returns
+ */
+export function confirmAddGroupApi(data) {
+	return request({
+		url: relationGroup + '/manage_join_group',
+		method: 'POST',
+		data
 	})
 }
--- src/pages/NewContact/NewContact.jsx
+++ src/pages/NewContact/NewContact.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { f7, Page, Navbar, List, ListItem, SwipeoutActions, SwipeoutButton, Button } from 'framework7-react'
-// import PropTypes from 'prop-types'
-import { friendApplyListApi, confirmAddFriendApi } from '@/api/relation'
-import { groupRequestListApi } from '@/api/group'
+import React, { useState } from 'react'
+import { Page, Navbar, Button, Subnavbar, Segmented, Block } from 'framework7-react'
+import { confirmAddFriendApi } from '@/api/relation'
+import { confirmAddGroupApi } from '@/api/group'
 import { useUserStore } from '@/stores/user'
 
 import { $t } from '@/i18n'
+import RequestList from './RequestList'
+import { useRelationRequestStore } from '@/stores/relationRequest'
 // import WebDB from '@/db'
 
+// import PropTypes from 'prop-types'
 // NewContact.propTypes = {
 // 	f7route: PropTypes.object
 // }
@@ -15,120 +17,81 @@ import { $t } from '@/i18n'
 export default function NewContact() {
 	const { directory } = useUserStore()
 
-	const [users, setUsers] = useState([])
-	useEffect(() => {
-		;(async () => {
-			groupRequestListApi()
-				.then(({ code, data }) => {
-					if (code !== 200) {
-						return
-					}
-					// TODO: 展示群请求列表
-					console.log(data)
-				})
-				.catch((err) => {
-					console.log(err)
-				})
-			const res = await friendApplyListApi()
-			if (res.code !== 200) return
-			let newUsers = res.data
-			console.log(newUsers)
-			if (!res.data) {
-				newUsers = []
-			} else {
-				newUsers = newUsers?.map((user) => {
-					return {
-						...user,
-						// 0 初始状态 1 已同意 2 已拒绝
-						status: 0
-					}
-				})
-			}
-			setUsers(newUsers)
-		})()
-	}, [])
+	// 选项卡
+	const { friendResquest, updateFriendResquest, groupResquest, updateGroupResquest } = useRelationRequestStore() // 获取申请列表
+	const tabbar = [
+		{
+			key: 'friend',
+			title: $t('联系人'),
+			data: friendResquest || [],
+			onClick: () => setActiveTabbar(0),
+			itemConfirm: async (id, status) => {
+				// 同意或拒绝添加好友
+				const { code } = await confirmAddFriendApi({ user_id: id, e2e_public_key: directory, status })
+				if (code !== 200) return
 
-	const text = {
-		tips: $t('对方没有留言'),
-		btn_agree: $t('同意'),
-		btn_refuse: $t('拒绝'),
-		agree: $t('已同意'),
-		refuse: $t('已拒绝')
-	}
-
-	// 同意或拒绝添加好友
-	const confirm = async (id, status) => {
-		const res = await confirmAddFriendApi({ user_id: id, e2e_public_key: directory, status })
-		if (res.code !== 200) return
-
-		const newUsers = users.map((user) => {
-			if (user.user_id === id) {
-				return {
-					...user,
+				// 修改状态
+				const newUsers = friendResquest.map((user) => {
 					// 0 初始状态 1 已同意 2 已拒绝
-					status: status === 0 ? 2 : 1
-				}
+					return user.user_id === id ? { ...user, status: status === 0 ? 2 : 1 } : user
+				})
+
+				// TODO: 添加好友到本地
+				// WebDB.contacts.add({  })
+
+				updateFriendResquest(newUsers)
 			}
-			return user
-		})
+		},
+		{
+			key: 'group',
+			title: $t('群聊'),
+			data: groupResquest || [],
+			onClick: () => setActiveTabbar(1),
+			itemConfirm: async (id, status, group_id) => {
+				// 同意或拒绝加入群聊
+				const { code } = await confirmAddGroupApi({
+					group_id,
+					user_id: id,
+					status // 1 同意 0 拒绝
+				})
+				if (code !== 200) return
 
-		// todo: 添加好友到本地
-		// WebDB.contacts.add({  })
+				// 修改状态
+				const newGroups = groupResquest.map((group) => {
+					// 0 初始状态 1 已同意 2 已拒绝
+					return group.user_id === id ? { ...group, status: status === 0 ? 2 : 1 } : group
+				})
 
-		setUsers(newUsers)
-	}
+				// TODO: 添加好友到本地
+				// WebDB.contacts.add({  })
+
+				updateGroupResquest(newGroups)
+			}
+		}
+	]
+	const [activeTabbar, setActiveTabbar] = useState(0)
 
 	return (
 		<Page noToolbar className="new-contact">
-			<Navbar title={$t('新请求')} backLink="Back" backLinkShowText="" />
-			<List noChevron dividers mediaList className="my-0">
-				{users.map((chat) => (
-					<ListItem
-						key={chat.user_id}
-						title={chat.nickname}
-						swipeout
-						// link
-					>
-						<img
-							slot="media"
-							src={chat.avatar}
-							loading="lazy"
-							alt={chat.nickname}
-							className="w-10 h-10 rounded-full"
-						/>
-						<span slot="text" className="text-gray-500 text-sm">
-							{chat?.msg || text.tips}
-						</span>
-
-						{chat.status === 1 ? (
-							<span className="text-gray-500 text-sm whitespace-nowrap pr-2" slot="content">
-								{text.agree}
-							</span>
-						) : chat.status === 2 ? (
-							<span className="text-gray-500 text-sm whitespace-nowrap pr-2" slot="content">
-								{text.refuse}
-							</span>
-						) : (
-							<div slot="content" className="pr-2 flex">
-								<Button className="text-sm text-red-500" onClick={() => confirm(chat?.user_id, 0)}>
-									{text.btn_refuse}
-								</Button>
-								<Button className="text-sm text-primary" onClick={() => confirm(chat?.user_id, 1)}>
-									{text.btn_agree}
-								</Button>
-							</div>
-						)}
-
-						<SwipeoutActions right>
-							{/* onClick={del} */}
-							<SwipeoutButton close color="red">
-								{/* <Icon f7="ellipsis" /> */}
-								<span>删除</span>
-							</SwipeoutButton>
-						</SwipeoutActions>
-					</ListItem>
-				))}
-			</List>
+			<Navbar title={$t('新请求')} backLink="Back" backLinkShowText="">
+				<Subnavbar>
+					<Segmented strong>
+						{tabbar.map((item, index) => (
+							<Button
+								key={index}
+								smallMd
+								active={item.key === tabbar[activeTabbar].key}
+								onClick={item.onClick}
+							>
+								{item.title}
+							</Button>
+						))}
+					</Segmented>
+				</Subnavbar>
+			</Navbar>
+			<Block>
+				<RequestList listData={tabbar[activeTabbar].data} confirm={tabbar[activeTabbar].itemConfirm} />
+			</Block>
 		</Page>
 	)
 }
