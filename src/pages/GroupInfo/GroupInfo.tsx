03e5d10d8ed3d63144179ffa18f7aa8d2c6341db
minisailboat <minisailboat@qq.com>
2024-02-27 11:06:34 +0800

    fix：群成员信息列表修改

--- src/pages/GroupInfo/GroupInfo.tsx
+++ src/pages/GroupInfo/GroupInfo.tsx
@@ -7,7 +7,7 @@ import { getCookie } from '@/utils/cookie'
 const user_id = getCookie(USER_ID) || ''
 
 interface GroupInfoProps {
-    group_id: string
+	group_id: string
 }
 
 const GroupInfo: React.FC<GroupInfoProps & RouterProps> = (props) => {
@@ -22,31 +22,31 @@ const GroupInfo: React.FC<GroupInfoProps & RouterProps> = (props) => {
 
 	// 群聊信息
 	const [groupInfo, setGroupInfo] = useState({
-        name: ""
-    })
+		name: ''
+	})
 	useEffect(() => {
-        (async () => {
-            // 获取群聊信息
-            const { code, data } = await GroupService.groupInfoApi({ group_id: GroupId })
-            if (code !== 200) return
-            console.log(data)
-            code === 200 && setGroupInfo(data)
-            const isSilence = data?.preferences?.silent_notification
-            setSilence(isSilence === 1)
-        })()
+		;(async () => {
+			// 获取群聊信息
+			const { code, data } = await GroupService.groupInfoApi({ group_id: GroupId })
+			if (code !== 200) return
+			console.log(data)
+			code === 200 && setGroupInfo(data)
+			const isSilence = data?.preferences?.silent_notification
+			setSilence(isSilence === 1)
+		})()
 	}, [])
 
 	// 群成员
 	const [members, setMembers] = useState([])
 	useEffect(() => {
-        (async () => {
+		;(async () => {
 			// 获取成员信息
-            const { code, data } = await GroupService.groupMemberApi({ group_id: GroupId })
-            if (code !== 200) return
-            const identity = data.find((i: any) => i.user_id === user_id).identity
-            identity && setIdentity(identity)
-            setMembers(data)
-        })()
+			const { code, data } = await GroupService.groupMemberApi({ group_id: GroupId })
+			if (code !== 200) return
+			const identity = data.find((i: any) => i.user_id === user_id).identity
+			identity && setIdentity(identity)
+			setMembers(data)
+		})()
 	}, [props])
 
 	// 退出群聊
@@ -104,33 +104,43 @@ const GroupInfo: React.FC<GroupInfoProps & RouterProps> = (props) => {
 			<List className="m-0 mb-3 bg-white" strong dividers outline>
 				<ListItem noChevron>
 					<div className="w-full flex flex-col justify-center items-center">
-						<div className="mb-6 w-full grid grid-cols-5 gap-6">
+						<div className="mb-1 w-full grid grid-cols-5 gap-6">
 							{members.slice(0, 23).map((item: any) => (
-								<div key={item.user_id} className="w-12 h-12 flex flex-col items-center">
-									<img className="rounded-lg" src={item.avatar} alt="" />
-									<div className="text-sm text-gray-400">{item.nickname}</div>
+								<div key={item.user_id} className="w-12 h-[144rpx] flex flex-col items-center">
+									<div className="min-w-12 min-h-12 bg-gray-200 rounded-lg overflow-hidden">
+										<img src={item.avatar} alt="" />
+									</div>
+									<div className="w-12 min-w-12 min-h-6 flex-1 text-sm text-gray-400 max-w-12 overflow-hidden text-ellipsis">
+										{item.nickname}
+									</div>
 								</div>
 							))}
 							<div
 								className="w-12 h-12 flex flex-col justify-center items-center border-dashed border-[1px] border-gray-400 text-gray-500 rounded-lg"
-                                onClick={() => f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.NOTMEMBER}`)}
+								onClick={() =>
+									f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.NOTMEMBER}`)
+								}
 							>
 								<Icon f7="plus" size={18} />
 							</div>
 							{identity && (
 								<div
 									className="w-12 h-12 flex flex-col justify-center items-center border-dashed border-[1px] border-gray-400 text-gray-500 rounded-lg"
-                                    onClick={() => f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.MEMBER}`)}
+									onClick={() =>
+										f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.MEMBER}`)
+									}
 								>
 									<Icon f7="minus" size={18} />
 								</div>
 							)}
 						</div>
-						{(members.length > (0)) && (
+						{members.length > 0 && (
 							<div className="mb-1">
 								<span
 									className="text-sm text-gray-500"
-                                    onClick={() => f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.MEMBERSHOW}`)}
+									onClick={() =>
+										f7router.navigate(`/group_info/${GroupId}/member/${MemberListType.MEMBERSHOW}`)
+									}
 								>
 									查看更多群成员
 								</span>
