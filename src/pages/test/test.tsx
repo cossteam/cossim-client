7a5ef28578cbbf5fdf2aa6fd668bdf5b3c72ba21
ocean <oceanchiu@foxmail.com>
2024-03-14 19:51:15 +0800

    feat: iOS配置文件

---
+++ src/pages/test/test.tsx
@@ -0,0 +1,28 @@
+
+import { $t } from '@/shared'
+import { List, NavTitle, Navbar, Page } from 'framework7-react'
+
+const Test: React.FC<RouterProps> = () => {
+
+
+	return (
+		<Page className="group-page" noToolbar messagesContent>
+			<Navbar className="messages-navbar" backLink>
+				<NavTitle>
+					<div className="">{$t('测试')}</div>
+				</NavTitle>
+			</Navbar>
+			<List contactsList noChevron dividers>
+				测试页面
+				<div  onTouchEnd={() => alert('结束触摸')} onDoubleClick={() => {
+					alert('双击');
+					console.log('双击')
+				}}>
+					button
+				</div>
+			</List>
+		</Page>
+	)
+}
+
+export default Test
