8b291bf654f0f78aadd4b70afa11129855b737da
ocean <oceanchiu@foxmail.com>
2024-03-21 11:00:35 +0800

    feat: 优化输入组件

--- src/components/CommInput/CommInput.tsx
+++ src/components/CommInput/CommInput.tsx
@@ -1,21 +1,27 @@
-import { useState } from "react"
-
-const CommInput = ({ title, onChange }: { title: string, onChange: Function }) => {
-    const [value, setValue] = useState('');
+import { useEffect, useState } from "react"
+import './CommInput.scss'
+import { Icon } from "framework7-react";
+const CommInput = ({ title, defaultValue, clearButton = true, type = 'text', onChange }: 
+    { title: string, defaultValue: string | number, clearButton?: boolean, type?: string, onChange: Function }) => {
+    const [value, setValue] = useState<string | number>();
+    useEffect(() => {
+        setValue(defaultValue)
+    }, [defaultValue])
     const handleClear = () => {
         setValue('');
     };
+
     return (
         <div className='custom-input-container flex flex-col px-5 gap-y-1'>
             <span className='ml-3 text-gray-700'>{title}</span>
-            <div className="w-full flex">
-                <input value={value} onChange={(e) => {
+            <div className="w-full">
+                <input value={value} type={type} onChange={(e) => {
                     onChange(e.target.value)
                     setValue(e.target.value)
-                }} className='rounded-md h-10 w-full bg-gray-100 px-3'></input>
-                {value && ( // 仅在输入框有值时显示清除按钮
-                    <button className="clear-button w-4 h-4 text-xs rounded-full text-white bg-gray-300" style={{ position: 'relative', right: '25px', top: '12px'  }} onClick={handleClear}>
-                        X
+                }} className='input' />
+                {value && clearButton && ( // 仅在输入框有值时显示清除按钮
+                    <button className="clear-button w-4 h-4 text-xs rounded-full text-white bg-gray-300" onClick={handleClear}>
+                        <Icon size={11} f7="xmark"/>
                     </button>
                 )}
             </div>
