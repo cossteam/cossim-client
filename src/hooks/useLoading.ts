a91077a3a761cd0294795462c025eb05a8d7b033
ocean <oceanchiu@foxmail.com>
2024-03-25 10:07:25 +0800

    feat: 异步加载动画钩子

---
+++ src/hooks/useLoading.ts
@@ -0,0 +1,23 @@
+import { $t } from "@/shared"
+import { f7 } from "framework7-react"
+import { useState } from "react"
+
+function useRequesLoading() {
+    const [loading, setLoading] = useState(false);
+    // 异步加载钩子
+    async function watchAsyncFn<T>(fn: (...args: any[]) => Promise<T>, content: string = '加载中...') {
+        f7.dialog.preloader($t(content))
+        setLoading(true)
+        return fn().finally(() => {
+            setLoading(false)
+            f7.dialog.close()
+        })
+    }
+
+    return {
+        loading,
+        watchAsyncFn
+    }
+}
+
+export default useRequesLoading
