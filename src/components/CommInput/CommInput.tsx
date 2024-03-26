ee71545ca82edbd7141971a93b630ac809a4d683
minisailboat <minisailboat@qq.com>
2024-03-26 09:58:11 +0800

    fix：修改 CommInput

--- src/components/CommInput/CommInput.tsx
+++ src/components/CommInput/CommInput.tsx
@@ -1,37 +1,50 @@
-import { InputHTMLAttributes, useState } from "react"
+import { InputHTMLAttributes, useState } from 'react'
 import './CommInput.scss'
-import { Icon } from "framework7-react";
+import { Icon } from 'framework7-react'
 
 interface CommInputProps {
-    title?: string;
-    clearButton?: boolean;
-    onChange?: (value: any) => any
+	title?: string
+	clearButton?: boolean
+	onChange?: (value: any) => any
 }
 
-const CommInput: React.FC<CommInputProps & InputHTMLAttributes<HTMLInputElement>> = ({ clearButton = true, onChange, ...resetProps }) => {
-    const [value, setValue] = useState(resetProps.defaultValue);
+const CommInput: React.FC<CommInputProps & InputHTMLAttributes<HTMLInputElement>> = ({
+	clearButton = true,
+	defaultValue,
+	onChange,
+	...resetProps
+}) => {
+	const [value, setValue] = useState(defaultValue)
 
-    const handleClear = () => {
-        setValue('');
-    };
+	const handleClear = () => {
+		setValue('')
+	}
 
-    return (
-        <div className='custom-input-container flex flex-col px-5 gap-y-1 mt-3'>
-            <span className='ml-3 text-gray-700'>{resetProps?.title}</span>
-            <div className="w-full input">
-                <input value={value} onChange={(e) => {
-                    onChange && onChange(e.target.value)
-                    setValue(e.target.value)
-                }} {...resetProps} className='input' />
-                {value && clearButton && ( // 仅在输入框有值时显示清除按钮
-                    <button className="clear-button w-4 h-4 text-xs rounded-full text-white bg-gray-300" onClick={handleClear}>
-                        <Icon size={11} f7="xmark"/>
-                    </button>
-                )}
-            </div>
-
-        </div>
-    )
+	return (
+		<div className="custom-input-container flex flex-col px-5 gap-y-1 mt-3">
+			<span className="ml-3 text-gray-700">{resetProps?.title}</span>
+			<div className="w-full input">
+				<input
+					value={value}
+					onChange={(e) => {
+						onChange && onChange(e.target.value)
+						setValue(e.target.value)
+					}}
+					{...resetProps}
+					className="input"
+				/>
+				{value &&
+					clearButton && ( // 仅在输入框有值时显示清除按钮
+						<button
+							className="clear-button w-4 h-4 text-xs rounded-full text-white bg-gray-300"
+							onClick={handleClear}
+						>
+							<Icon size={11} f7="xmark" />
+						</button>
+					)}
+			</div>
+		</div>
+	)
 }
 
 export default CommInput
