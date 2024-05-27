e9f14eec0bc9fca5cff5b44563db071b5c828af9
minisailboat <minisailboat@qq.com>
2024-05-27 17:44:26 +0800

    fix：修改鉴权 hook 名称

--- src/app.tsx
+++ src/app.tsx
@@ -5,12 +5,12 @@ import Loading from '@/components/loading'
 import { useRoutes } from 'react-router'
 import routes from '~react-pages'
 import { App as AppComponent } from 'antd'
-import useLogin from '@/hooks/useLogin'
+import useAuth from '@/hooks/useLogin'
 
 const App = memo(() => {
 	const commonStore = useCommonStore()
 	// 鉴权
-	useLogin()
+	useAuth()
 	return (
 		<ConfigProvider theme={{ token: { colorPrimary: commonStore.themeColor, borderRadius: 4, fontSize: 16 } }}>
 			<AppComponent>
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,7 +2,7 @@ import useUserStore from '@/stores/user'
 import { useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router'
 
-function useLogin() {
+function useAuth() {
 	const userStore = useUserStore()
 	const location = useLocation()
 	const navigate = useNavigate()
@@ -23,4 +23,4 @@ function useLogin() {
 	}, [userStore.token, location])
 }
 
-export default useLogin
+export default useAuth
