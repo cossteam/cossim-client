74ac842eec76810e4b668c4f91ab6293611e5839
minisailboat <minisailboat@qq.com>
2024-05-29 14:05:32 +0800

    feat: 添加用户相关接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,198 +1,162 @@
-// // import type {
-// // 	LoginData,
-// // 	PublicKeyData,
-// // 	RegisterData,
-// // 	SearchUserParams,
-// // 	UpdateUserInfData,
-// // 	UserInfoParams,
-// // 	updatePassWorData,
-// // 	LogoutData
-// // } from '@/types/api/user'
-// import request from '@/utils/request'
+import type {
+	LoginParams,
+	LogoutParams,
+	PassWordUpdateParams,
+	PublicKeyParams,
+	RegisterParams,
+	SearchUserParams,
+	UserInfUpdateParams,
+	UserInfoQueryParams,
+	UserPublicKeyQueryParams
+} from '@/types/api'
+import request from '@/utils/request'
 
-// class UserServiceImpl {
-// 	private baseUrl: string = '/user'
+const baseUrl: string = '/user'
 
-// 	/**
-// 	 * 使用提供的数据发送登录请求。
-// 	 *
-// 	 * @param {LoginData} data
-// 	 * @return {Promise<ResponseData>}
-// 	 */
-// 	loginApi(data: LoginData): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/login`,
-// 			method: 'POST',
-// 			data
-// 		})
-// 	}
+/**
+ * 登录接口
+ * @param data
+ * @returns
+ */
+export function loginApi(data: LoginParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/login`,
+		method: 'POST',
+		data
+	})
+}
 
-// 	/**
-// 	 * 注册接口
-// 	 *
-// 	 * @param {RegisterData} data
-// 	 * @returns {Promise<ResponseData>}
-// 	 */
-// 	registerApi(data: RegisterData): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/register`,
-// 			method: 'POST',
-// 			data
-// 		})
-// 	}
+/**
+ * 注册接口
+ * @param data
+ * @returns
+ */
+export function registerApi(data: RegisterParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/register`,
+		method: 'POST',
+		data
+	})
+}
 
-// 	/**
-// 	 * 上传公钥到服务器
-// 	 * @param {PublicKeyData} data
-// 	 * @returns {Promise<ResponseData>}
-// 	 */
-// 	uploadPublicKeyApi(data: PublicKeyData): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/bundle`,
-// 			method: 'PUT',
-// 			data
-// 		})
-// 	}
+/**
+ * 修改用户密钥包（消息加密）
+ * @param data
+ * @returns
+ */
+export function uploadPublicKeyApi(data: PublicKeyParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/bundle`,
+		method: 'PUT',
+		data
+	})
+}
 
-// 	// setPgpKeyApi(data) {
-// 	// 	return request({
-// 	// 		url: `${baseApi}/key/set`,
-// 	// 		method: 'post',
-// 	// 		data
-// 	// 	})
-// 	// }
+/**
+ * 搜索用户
+ * @param params
+ * @returns
+ */
+export function searchUserApi(params: SearchUserParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/search`,
+		method: 'GET',
+		params
+	})
+}
 
-// 	// /**
-// 	//  * 交换E2EKey
-// 	//  * @param {*} data
-// 	//  * @param {*} data.public_key
-// 	//  * @param {*} data.user_id
-// 	//  * @returns
-// 	//  */
-// 	// switchE2EKey(data) {
-// 	// 	return request({
-// 	// 		url: `${baseApi}/switch/e2e/key`,
-// 	// 		method: 'POST',
-// 	// 		data
-// 	// 	})
-// 	// }
+/**
+ * 查询用户信息
+ * @param params
+ * @returns
+ */
+export function getUserInfoApi(params: UserInfoQueryParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/${params.id}`,
+		method: 'GET',
+		params
+	})
+}
 
-// 	/**
-// 	 * 搜索用户
-// 	 * @param {*} params
-// 	 * @param {*} params.email
-// 	 * @returns
-// 	 */
-// 	searchUserApi(params: SearchUserParams): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/search`,
-// 			params
-// 		})
-// 	}
+/**
+ * 修改用户信息
+ * @param data
+ * @returns
+ */
+export function updateUserInfoApi(data: UserInfUpdateParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}`,
+		method: 'PUT',
+		data
+	})
+}
 
-// 	/**
-// 	 * 查询用户信息
-// 	 * @param {*} params
-// 	 * @param {string} params.user_id     	用户 邮箱 | id
-// 	 * @returns
-// 	 */
-// 	getUserInfoApi(params: UserInfoParams): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/${params.user_id}`,
-// 			method: 'GET'
-// 			// params
-// 		})
-// 	}
+/**
+ * 修改密码
+ * @param data
+ * @returns
+ */
+export function updatePassWordApi(data: PassWordUpdateParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/password`,
+		method: 'PUT',
+		data
+	})
+}
 
-// 	/**
-// 	 * 修改用户信息
-// 	 * @param {UpdateUserInfData} data
-// 	 * @returns
-// 	 */
-// 	updateUserInfoApi(data: UpdateUserInfData): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}`,
-// 			method: 'PUT',
-// 			data
-// 		})
-// 	}
+/**
+ * 退出登录
+ * @returns
+ */
+export function logoutApi(data: LogoutParams) {
+	return request({
+		url: `${baseUrl}/logout`,
+		method: 'POST',
+		data
+	})
+}
 
-// 	/**
-// 	 * 修改密码
-// 	 * @param {*} data
-// 	 * @param {*} data.old_password
-// 	 * @param {*} data.password
-// 	 * @param {*} data.confirm_password
-// 	 * @returns
-// 	 */
-// 	updatePassWordApi(data: updatePassWorData): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/password`,
-// 			method: 'PUT',
-// 			data
-// 		})
-// 	}
+/**
+ * 修改用户头像
+ * @param {*} data.file - 用户头像文件
+ * @returns {Promise<ResponseData>}
+ */
+export function updateAvatarApi(data: { file: File }): Promise<ResponseData> {
+	const formData = new FormData()
+	formData.append('file', data.file)
+	return request({
+		url: `${baseUrl}/avatar`,
+		method: 'PUT',
+		data: formData,
+		headers: {
+			'Content-Type': 'multipart/form-data'
+		}
+	})
+}
 
-// 	/**
-// 	 * 退出登录
-// 	 * @returns
-// 	 */
-// 	logoutApi(data: LogoutData) {
-// 		return request({
-// 			url: `${this.baseUrl}/logout`,
-// 			method: 'POST',
-// 			data
-// 		})
-// 	}
+/**
+ * 获取用户的公钥
+ * @param params
+ * @returns
+ */
+export function getPublicKeyApi(params: UserPublicKeyQueryParams): Promise<ResponseData> {
+	return request({
+		url: `${baseUrl}/${params.user_id}/bundle`,
+		method: 'GET',
+		params
+	})
+}
 
-// 	/**
-// 	 * 修改用户头像
-// 	 * @param {*} data
-// 	 * @param {*} data.file - 用户头像文件
-// 	 * @returns {Promise<ResponseData>}
-// 	 */
-// 	updateAvatarApi(data: { file: File }): Promise<ResponseData> {
-// 		const formData = new FormData()
-// 		formData.append('file', data.file)
-
-// 		return request({
-// 			url: `${this.baseUrl}/avatar`,
-// 			method: 'PUT',
-// 			data: formData,
-// 			headers: {
-// 				'Content-Type': 'multipart/form-data'
-// 			}
-// 		})
-// 	}
-
-// 	/**
-// 	 * 获取用户的公钥
-// 	 * @param {*} params
-// 	 * @param {*} params.user_id
-// 	 */
-// 	getPublicKeyApi(params: UserInfoParams): Promise<ResponseData> {
-// 		return request({
-// 			url: `${this.baseUrl}/${params.user_id}/bundle`,
-// 			method: 'GET'
-// 			// params
-// 		})
-// 	}
-
-// 	// /**
-// 	//  * 修改自己的公钥
-// 	//  * @param {*} data
-// 	//  * @param {*} data.secret_bundle
-// 	//  * @returns
-// 	//  */
-// 	// updatePublicKeyApi(data) {
-// 	// 	return request({
-// 	// 		url: `${baseApi}/bundle/modify`,
-// 	// 		method: 'POST',
-// 	// 		data
-// 	// 	})
-// 	// }
+// /**
+//  * 修改自己的公钥
+//  * @param {*} data
+//  * @param {*} data.secret_bundle
+//  * @returns
+//  */
+// export function updatePublicKeyApi(data) {
+// 	return request({
+// 		url: `${baseApi}/bundle/modify`,
+// 		method: 'POST',
+// 		data
+// 	})
 // }
-
-// const UserService = new UserServiceImpl()
-
-// export default UserService
