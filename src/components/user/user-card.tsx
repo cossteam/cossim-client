bf088ec39946333d9c66d713e58b40c36172a47a
am6737 <91421697+am6737@users.noreply.github.com>
2024-06-07 19:46:29 +0800

    pref(user): 优化联系人信息页面和联系人列表 (#39)

--- src/components/user/user-card.tsx
+++ src/components/user/user-card.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, List, Typography } from 'antd'
+import { Avatar, Card, Divider, List, Switch, Typography } from 'antd'
 import { UserAddOutlined, EllipsisOutlined, CopyOutlined } from '@ant-design/icons'
 
 const { Meta } = Card
@@ -31,11 +31,11 @@ const UserCard: React.FC<UserCardProps> = ({ avatar, title, description, coverIm
                         <List.Item key={item.title} className="p-2">
                             <List.Item.Meta
                                 title={
-                                    <Typography.Text type="secondary" className="text-xs leading-tight text-gray-500">
+                                    <Typography.Text type="secondary" className="text-sm leading-tight text-gray-500">
                                         {item.title}
                                     </Typography.Text>
                                 }
-                                description={<Typography className="text-xs leading-tight">{item.content}</Typography>}
+                                description={<Typography className="text-sm leading-tight">{item.content}</Typography>}
                             />
                             {item.action && (
                                 <Typography.Text
@@ -55,9 +55,47 @@ const UserCard: React.FC<UserCardProps> = ({ avatar, title, description, coverIm
                 />
             </div>
 
-            <div className="mt-2 h-[40px] bg-white text-xs flex items-center justify-between px-4">
-                <Typography.Text className="text-xs leading-tight">共同的群</Typography.Text>
-                <Typography.Text className="text-xs leading-tight text-gray-500">{groupCount}</Typography.Text>
+            <div className="mt-2 bg-white flex flex-col justify-between">
+                <div>
+                    <div className="p-4 flex justify-between">
+                        <Typography.Text className="text-sm leading-tight">共同的群</Typography.Text>
+                        <Typography.Text className="text-sm leading-tight text-gray-500">{groupCount}</Typography.Text>
+                    </div>
+                    <Divider className="m-0" />
+                </div>
+
+                <div>
+                    <div className="p-4 flex justify-between">
+                        <Typography.Text className="text-sm leading-tight">置顶聊天</Typography.Text>
+                        {/* <Typography.Text className="text-sm leading-tight text-gray-500">{groupCount}</Typography.Text> */}
+                        <Switch />
+                    </div>
+                    <Divider className="m-0" />
+                </div>
+
+                <div>
+                    <div className="p-4 flex justify-between">
+                        <Typography.Text className="text-sm leading-tight">消息免打扰</Typography.Text>
+                        <Switch />
+                    </div>
+                    <Divider className="m-0" />
+                </div>
+
+                <div>
+                    <div className="p-4 flex justify-between">
+                        <Typography.Text className="text-sm leading-tight">阅后即焚</Typography.Text>
+                        <Switch />
+                    </div>
+                    <Divider className="m-0" />
+                </div>
+
+                <div>
+                    <div className="p-4 flex justify-between">
+                        <Typography.Text className="text-sm leading-tight">添加到黑名单</Typography.Text>
+                        <Switch />
+                    </div>
+                    <Divider className="m-0" />
+                </div>
             </div>
         </div>
     )
