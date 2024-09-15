3faa402dcc64fce9f5c696829c8dc66148c7de8f
lms <1359816810@qq.com>
2024-09-15 14:44:38 +0800

    fix: 修复使用遗留的store导致清空登录信息

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,5 +1,8 @@
-import useUserStore from '@/stores/user'
+import { useAuthStore } from '@/stores/auth'
+// import useUserStore from '@/stores/user'
 
-export const isSelf = (userId: string) => userId === useUserStore.getState().userId
+// export const isSelf = (userId: string) => userId === useUserStore.getState().userId
+export const isSelf = (userId: string) => userId === useAuthStore.getState().userId
+
 
 export const isGroupDialog = (dialog: ChatData) => !!dialog.group_id
