03fd702fdfc7c41dfe25a7f8bf09b6e4b203605c
am6737 <1359816810@qq.com>
2024-10-04 18:55:22 +0800

    t1

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 // import './styles/base.css'
 import './styles/base.scss'
 import App from './app'
+// t1
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
